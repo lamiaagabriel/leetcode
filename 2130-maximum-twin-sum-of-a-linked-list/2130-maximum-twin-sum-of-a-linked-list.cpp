edb70bb85dc8af094241112da22a108f7e587c39
Lamiaa Gabriel <64805908+lamiaagabriel@users.noreply.github.com>
2023-07-13 18:52:05 +0300

    Time: 338 ms (5.01%), Space: 127.3 MB (5.20%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,33 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        ListNode *cursor = head, *reversed = nullptr;
-        int max = 0;
+        stack<int> s;
+        queue<int> q;
+        int _max = 0;
         
         
-        while(cursor != nullptr) {
-            reversed = new ListNode(cursor->val, reversed);
-            cursor = cursor->next;
-        }
-        
-        while(!(head == nullptr || reversed == nullptr)) {
-            if(max < head->val + reversed->val)
-                max = head->val + reversed->val;
+        while(head != nullptr) {
+            s.push(head->val);
+            q.push(head->val);
             
             head = head->next;
-            reversed = reversed->next;
         }
         
-        return max;
+        while (!(s.empty() || q.empty())) {
+            _max = std::max(_max, s.top() + q.front());
+            
+            s.pop();
+            q.pop();
+        }
+        
+        return _max;
     }
 };
