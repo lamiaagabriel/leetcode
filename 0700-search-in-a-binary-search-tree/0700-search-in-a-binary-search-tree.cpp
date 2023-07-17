bd57dff79c52e76ef8b1bd5061c22ba31c94c1f7
Lamiaa Gabriel <64805908+lamiaagabriel@users.noreply.github.com>
2023-07-17 03:18:59 +0300

    Time: 54 ms (12.86%), Space: 34.8 MB (35.00%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void searchInOrder(TreeNode* &root, TreeNode* &head, int val) {
+    void searchPreOrder(TreeNode* &root, TreeNode* &head, int val) {
         if (root == nullptr || head != nullptr) return;
         if(root->val == val) {
             head = root;
             return;
         }
 
-        searchInOrder(root->left, head, val);
-        searchInOrder(root->right, head, val);
+        searchPreOrder(root->left, head, val);
+        searchPreOrder(root->right, head, val);
     }
-
 
 
     TreeNode* searchBST(TreeNode* root, int val) {
         TreeNode* head = nullptr;
-        searchInOrder(root, head, val);
+        searchPreOrder(root, head, val);
         return head;
     }
 };
